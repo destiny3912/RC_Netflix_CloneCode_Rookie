a6fa83d06a1e017c0b1e6707dd725f47b0114c21
destiny3912 <destiny3910@gmail.com>
2023-02-19 18:19:30 +0900

    comment:

--- src/main/java/com/example/demo/src/user/UserProvider.java
+++ src/main/java/com/example/demo/src/user/UserProvider.java
@@ -14,20 +14,12 @@ import java.util.List;
 import static com.example.demo.config.BaseResponseStatus.*;
 
 //Provider : Read의 비즈니스 로직 처리
-@Service    // [Business Layer에서 Service를 명시하기 위해서 사용] 비즈니스 로직이나 respository layer 호출하는 함수에 사용된다.
-            // [Business Layer]는 컨트롤러와 데이터 베이스를 연결
-/**
- * Provider란?
- * Controller에 의해 호출되어 실제 비즈니스 로직과 트랜잭션을 처리: Read의 비즈니스 로직 처리
- * 요청한 작업을 처리하는 관정을 하나의 작업으로 묶음
- * dao를 호출하여 DB CRUD를 처리 후 Controller로 반환
- */
+@Service
 public class UserProvider {
 
 
-    // *********************** 동작에 있어 필요한 요소들을 불러옵니다. *************************
     private final UserDao userDao;
-    private final JwtService jwtService; // JWT부분은 7주차에 다루므로 모르셔도 됩니다!
+    private final JwtService jwtService;
 
 
     final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -35,11 +27,8 @@ public class UserProvider {
     @Autowired //readme 참고
     public UserProvider(UserDao userDao, JwtService jwtService) {
         this.userDao = userDao;
-        this.jwtService = jwtService; // JWT부분은 7주차에 다루므로 모르셔도 됩니다!
+        this.jwtService = jwtService;
     }
-
-
-    // ******************************************************************************
 
     public GetUserInfoRes getUserInfo(int userIdx) throws BaseException{
         try {
